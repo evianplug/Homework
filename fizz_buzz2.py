7df17e0d2540c7f359cbbf7fbfeaa96f7486a7e3
C4rt1 Kk1ll3r <kamuongua@gmail.com>
2020-07-25 03:28:03 +0300

    message

---
+++ fizz_buzz2.py
@@ -0,0 +1,440 @@
+# First Line
+file = open('text.txt')
+print("First line:")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# Second line
+print("\nSecond line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# Third line
+print("\nThird line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# Fourth line
+print("\nFourth line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# fifth line
+print("\nFifth line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# sixth line
+print("\nSixth line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# seventh line
+print("\nSeventh line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# eighth line
+print("\nEighth line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# nineth line
+print("\nNineth line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# tenth line
+print("\nTenth line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# eleventh line
+print("\nEleventh line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# twelveth line
+print("\nTwelveth line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# thirteenth line
+print("\nThirteenth line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# fourteenth line
+print("\nFourteenth line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# fifteenth line
+print("\nFifteenth line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# sixteenth line
+print("\nSixteenth line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# seventeenth line
+print("\nSeventeenth line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# eighteenth line
+print("\nEighteenth line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# nineteenth line
+print("\nNineteenth line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
+
+# twentieth line
+print("\nTwentieth line")
+print(file.readline())
+FB = []
+
+first = int(input("First number:"))
+second = int(input("Second number:"))
+third = int(input("Third number:"))
+for i in range(first, third + 1):
+    entry = ''
+    if i % 2 == 0:
+        entry += "F"
+    if i % 5 == 0:
+        entry += "B"
+    if i % 2 != 0 and i % 5 != 0:
+        entry = i
+
+    FB.append(entry)
+
+for i in FB:
+    print(i)
